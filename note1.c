8abd477d9fc36f150fb18299f3965dfc228caa98
Josh <shuozhang.josh@gmail.com>
2026-01-18 09:50:59 +0800

    Initial commit

---
+++ note1.c
@@ -0,0 +1,22 @@
+// stm32 F103C8T6
+
+    // important/special pins
+    // VDD: supply volatage
+    // VSS: ground
+    // NRST(Active low): reset
+    // VBAT: backup power supply for RTC and backup everthing when vdd is dead
+    // BOOt0: boot mode selection pin
+
+
+    // normal pins(functionality are similar)
+    // GPIO
+    // GPIO A PA0-PA15
+    // GPIO B PB0-PB15
+    // GPIO C PC13-PC15
+    // GPIO D PD0-PD1
+    // - Iputs
+    // - Outputs
+    // Output push-pull
+    // Output open-drain
+    // Alternate function push-pull
+    // Alternate function open-drain
