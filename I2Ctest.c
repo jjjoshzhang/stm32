1bd17615fc08e1abf17bb0a6d40a6defcc5a1871
Josh <shuozhang.josh@gmail.com>
2026-01-25 16:46:32 +0800

    i2c send data v1

--- I2Ctest.c
+++ I2Ctest.c
@@ -1,7 +1,15 @@
 
 #include "stm32f10x.h"
 
+
+// init stm32 I2C
 void My_I2C_Init(void);
+
+// send data from stm32 to slave devices
+int My_I2C_SendBytes(I2C_TypeDef *I2Cx, 
+  uint8_t Addr, // adress
+  uint8_t *pData, 
+  uint16_t Size);
 
 
 int main(void){
@@ -65,6 +73,55 @@ void My_I2C_Init(void){
 
 
 
+
+
+}
+
+
+int My_I2C_SendBytes(I2C_TypeDef *I2Cx, uint8_t Addr, uint16_t Size){
+  // wait till I2C bus becomes empty 
+  while(I2C_GetFlagStatus(I2Cx,I2C_FLAG_BUSY)== SET);
+
+  // START 
+  I2C_GenerateStart(I2Cx,ENABLE);
+  // wait till it's enabled, 0->1
+  while(I2C_GetFlagStatus(I2Cx,I2C_FLAG_SB) == RESET);
+  
+  //addressing 
+  // reset AF flag
+  I2C_ClearFlag(I2Cx,I2C_FlAG_AF);
+  // & 0xfe make it shift left 
+  I2C_SendData(I2Cx,Addr & 0xfe);
+
+  for(;;){
+    if(I2C_GetFlagStatus(I2Cx,I2C_FLAG_ADDR) == SET){
+      // addressing succesfully 
+      break;
+    } 
+    if(I2C_GetFlagStatus(I2Cx,I2C_FlAG_AF)== SET){
+      I2C_GenerateSTOP(I2Cx,ENABLE);
+      // addressing went wrong
+      return -1;
+    }
+  }
+
+  // clear addr flag
+  I2C_ReadRegister(I2Cx,I2C_Register_SR1);
+  I2C_ReadRegister(I2Cx,I2C_Register_SR2);
+// send data
+  for(uint16_t i =0; i<SIZE; i++){
+    while(1){
+      if(I2C_GetFlagStatus(I2Cx,I2C_FlAG_AF)== SET)
+{
+  I2C_GenerateSTOP(I2Cx,ENABLE);
+}    
+if(I2C_GetFlagStatus(I2Cx,I2C_FlAG_TXE)== SET){
+  break;
+} 
+}
+    
+
+  }
 
 
 }
