85097d3951bc2638eb1decbc6c48ef53577bfd65
Josh <shuozhang.josh@gmail.com>
2026-02-10 14:51:09 +0800

    i2c read data

--- I2Ctest.c
+++ I2Ctest.c
@@ -7,9 +7,15 @@ void My_I2C_Init(void);
 
 // send data from stm32 to slave devices
 int My_I2C_SendBytes(I2C_TypeDef *I2Cx, 
-  uint8_t Addr, // adress
+  uint8_t Addr, // address
   uint8_t *pData, 
   uint16_t Size);
+
+
+// return 0 means read sucessfully, -1 means addressing went wrong
+int My_I2C_ReceiveBytes(I2C_TypeDef *I2Cx, uint8_t Addr, uint8_t *pBuffer, uint16_t Size);
+
+
 
 
 int main(void){
@@ -94,8 +100,8 @@ int My_I2C_SendBytes(I2C_TypeDef *I2Cx, uint8_t Addr, uint16_t Size){
 
   // reset AF flag
   I2C_ClearFlag(I2Cx,I2C_FlAG_AF);
-  // & 0xfe make it shift left(only needed for sending addr)
-  // and we are sending addr now 
+  // set R/W = 0
+  // we are sending addr now 
   I2C_SendData(I2Cx,Addr & 0xfe);
 
 // infinite loop bc the code executes faster than I2C hardware
@@ -113,7 +119,7 @@ int My_I2C_SendBytes(I2C_TypeDef *I2Cx, uint8_t Addr, uint16_t Size){
 
 
   // clear addr flag
-  // clear it here bc we know that addressing is succesful
+  // clear it here bc we know that addressing is successful
   I2C_ReadRegister(I2Cx,I2C_Register_SR1);
   I2C_ReadRegister(I2Cx,I2C_Register_SR2);
 
@@ -155,4 +161,122 @@ I2C_SendData(I2Cx,pData[i]);
   return 0; // data was sent sucessfully 
 
 
+}
+
+
+
+
+int My_I2C_ReceiveBytes(I2C_TypeDef *I2Cx, uint8_t Addr, uint8_t *pBuffer, uint16_t Size)
+{ 
+  // set start bit to 1  
+  I2C_GenerateStart(I2Cx,ENABLE);
+
+  while(I2C_GetFlagStatus(I2Cx,I2C_FLAG_SB) == RESET);
+
+  // addressing 
+
+  // reset AF flag
+  I2C_ClearFlag(I2Cx,I2C_FlAG_AF);
+  // make R/W = 1
+  I2C_SendData(I2Cx, Addr | 0x01);
+
+
+  while(1)
+  {
+    if(I2C_GetFlagStatus(I2Cx,I2C_FLAG_AF) == SET)
+    {
+      I2C_GenerateSTOP(I2Cx,ENABLE);
+      return -1; // addressing went wrong 
+    }
+    if(I2C_GetFlagStatus(I2Cx,I2C_FlAG_TXE) == SET)
+    {
+      break;
+    } 
+  }
+
+  // receive data 
+
+// 1 byte
+  if(Size == 1)
+  {
+    // clear addr flag
+  
+    I2C_ReadRegister(I2Cx,I2C_Register_SR1);
+    I2C_ReadRegister(I2Cx,I2C_Register_SR2);
+
+    // set ACK to 0, STOP = 1
+    // "pre-program"
+    I2C_AcknowledgeConfig(I2Cx,DISABLE);
+
+    I2C_GenerateStop(I2Cx,ENABLE);
+
+    while(I2C_GetFlagStatus(I2Cx,I2C_FlAG_RXNE) == RESET);
+
+    pBuffer[0] = I2C_ReceiveData(I2Cx);
+
+  }else if(Size == 2)
+  {
+    // clear addr flag
+  
+    I2C_ReadRegister(I2Cx,I2C_Register_SR1);
+    I2C_ReadRegister(I2Cx,I2C_Register_SR2);
+
+    // set ACK to 1
+    I2C_AcknowledgeConfig(I2Cx,ENABLE);
+
+    // wait till the first data is ready to read
+
+    while(I2C_GetFlagStatus(I2Cx,I2C_FlAG_RXNE) == RESET);
+
+    pBuffer[0] = I2C_ReceiveData(I2Cx);
+
+
+     // set ACK to 0, STOP = 1 (for second data)
+    // "pre-program"
+    I2C_AcknowledgeConfig(I2Cx,DISABLE);
+
+    I2C_GenerateStop(I2Cx,ENABLE);
+
+    while(I2C_GetFlagStatus(I2Cx,I2C_FlAG_RXNE) == RESET);
+    // receive second data 
+    pBuffer[1] = I2C_ReceiveData(I2Cx);
+
+
+  }else
+  {
+    // clear addr flag
+  
+    I2C_ReadRegister(I2Cx,I2C_Register_SR1);
+    I2C_ReadRegister(I2Cx,I2C_Register_SR2);
+
+    // set ACK to 1
+    I2C_AcknowledgeConfig(I2Cx,ENABLE);
+
+    for(uint16_t i =0, i<Size-1, i++)
+    {
+
+
+      // wait till the first data is ready to read
+
+      while(I2C_GetFlagStatus(I2Cx,I2C_FlAG_RXNE) == RESET);
+
+      pBuffer[i] = I2C_ReceiveData(I2Cx);
+
+    }
+
+
+    I2C_AcknowledgeConfig(I2Cx,DISABLE);
+
+    I2C_GenerateStop(I2Cx,ENABLE);
+
+    while(I2C_GetFlagStatus(I2Cx,I2C_FlAG_RXNE) == RESET);
+ 
+    pBuffer[Size-1] = I2C_ReceiveData(I2Cx);
+
+  }
+
+
+  return 0;
+
+ 
 }
