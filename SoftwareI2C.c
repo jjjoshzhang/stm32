c9f6f6abd44afd898d0628dcd6725f8c93e3fdf5
Josh <shuozhang.josh@gmail.com>
2026-02-14 14:09:46 +0800

    i2c v1

--- SoftwareI2C.c
+++ SoftwareI2C.c
@@ -5,6 +5,12 @@
 void My_SI2C_Init(void);
 
 void scl_write(uint8_t level);
+void sda_write(uint8_t level);
+uint8_t sda_read(void);
+void delay_us(uint32_t us);
+
+void SendStart(void);
+void SendStop(void);
 
 
 int main(void)
@@ -21,7 +27,7 @@ int main(void)
 
 void My_SI2C_Init(void)
 {
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIO, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
     GPIO_InitTypeDef GPIO_InitStruct;
 
     GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
@@ -35,9 +41,55 @@ void My_SI2C_Init(void)
 
 void scl_write(uint8_t level)
 {
+    // PA0
     if(level == 0)
+    { 
+        GPI_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET);
+    }
+    else
     {
-        GPI_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET)
+        GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_SET)
     }
 
 }
+ 
+
+void sda_write(uint8_t level)
+{
+    // PA1
+    if(level == 0)
+    { 
+        GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET);
+    }
+    else
+    {
+        GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_SET)
+    }
+
+}
+uint8_t sda_read(void)
+{
+    if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_1) == Bit_SET)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+
+    }
+}
+void delay_us(uint32_t us){
+    // abt 8 loop iteration = 1us
+    uint32_t n = us * 8;
+    for(uint32_t i =0; i<n; i++);
+}
+
+
+void SendStart(void)
+{
+    sda_write(0);
+    delay_us(1); 
+}
+
+
