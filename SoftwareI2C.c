b622b825db8e7b35865a6cfd8628aa2a0cf50c99
Josh <shuozhang.josh@gmail.com>
2026-02-14 13:52:59 +0800

    soft i2c

--- SoftwareI2C.c
+++ SoftwareI2C.c
@@ -4,14 +4,14 @@
 // init stm32 SI2C
 void My_SI2C_Init(void);
 
+void scl_write(uint8_t level);
+
 
 int main(void)
 {
     while(1)
     {
-        RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIO, ENABLE);
-        GPIO_InitTypeDef GPIO_InitStruct;
-        GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+        My_SI2C_Init();
         
 
 
@@ -21,6 +21,23 @@ int main(void)
 
 void My_SI2C_Init(void)
 {
-    RCC
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIO, ENABLE);
+    GPIO_InitTypeDef GPIO_InitStruct;
+
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_OD;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
+
+    GPIO_Init(GPIOA,&GPIO_InitStruct);
+
+}
+
+
+void scl_write(uint8_t level)
+{
+    if(level == 0)
+    {
+        GPI_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET)
+    }
 
 }
